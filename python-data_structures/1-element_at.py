197761d8d8f72066600f5af783898d8ed926a774
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-13 16:17:30 +0400

    Refactor element_at function by removing comments
    Removed unnecessary comments and simplified the function.

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-    """Siyahıdan verilmiş indeksdəki elementi qaytarır."""
-    # İndeks mənfidirsə və ya siyahının uzunluğundan böyükdürsə
     if idx < 0 or idx >= len(my_list):
         return None
-    
-    # Əks halda elementi qaytar
     return my_list[idx]
