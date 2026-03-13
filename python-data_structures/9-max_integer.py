c53dfb576645015f7f024206bf359211a4ea6068
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-13 16:25:36 +0400

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,19 +1,12 @@
-Write a function that finds the biggest integer of a list.
-
-    Prototype: def max_integer(my_list=[]):
-    If the list is empty, return None
-    You can assume that the list only contains integers
-    You are not allowed to import any module
-    You are not allowed to use the builtin max()
-
-guillaume@ubuntu:~/$ cat 9-main.py
 #!/usr/bin/python3
-max_integer = __import__('9-max_integer').max_integer
+def max_integer(my_list=[]):
+    """Finds the biggest integer of a list."""
+    if not my_list:
+        return None
 
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
+    biggest = my_list[0]
+    for i in my_list:
+        if i > biggest:
+            biggest = i
 
-guillaume@ubuntu:~/$ ./9-main.py
-Max: 90
-guillaume@ubuntu:~/$ 
+    return biggest
