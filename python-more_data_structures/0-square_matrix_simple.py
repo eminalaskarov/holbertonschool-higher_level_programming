3ef3a8cca40c100e52da3cebab65f5d37346e363
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-13 16:42:04 +0400

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """Matrisin hər bir elementinin kvadratından ibarət yeni matris yaradır."""
-    # Xarici list comprehension hər bir sətir üçün, 
-    # daxili olan isə hər element üçün kvadrat hesablayır.
+    """Computes the square value of all integers of a matrix."""
     return [[x**2 for x in row] for row in matrix]
