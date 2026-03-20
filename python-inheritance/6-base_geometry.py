11ff80808eee732ee728cd2c8047479c9305219b
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-20 19:28:30 +0400

    Update and rename 5-base_geometry.py to 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+6-base geometry module with class BaseGeomertry
+'''
+
+
+class BaseGeometry:
+    '''
+    BaseGeometry class
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
