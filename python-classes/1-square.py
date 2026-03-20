4c0c936b6cb70de2df22fb4841da7601504c0966
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-20 18:59:03 +0400

    Update and rename 0-square.py to 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module with Square class
+"""
+
+
+class Square:
+    """
+    Square class that contains nothing rn
+    """
+    pass
+#!/usr/bin/python3
+"""
+Module with Square class
+"""
+
+
+class Square:
+    """
+    Square class that contains private size attribute
+    """
+    def __init__(self, size):
+        """
+        initilaize method
+        """
+        self.__size = size
