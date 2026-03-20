dfc0a6d5fe6569a815c6e8e649f9a6099181a3a5
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-20 18:09:16 +0400

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
+"""
+Bu modul 'add_integer' funksiyasını ehtiva edir.
+Modulun məqsədi iki ədədi təhlükəsiz şəkildə toplamaqdır.
+"""
+
+
 def add_integer(a, b=98):
+    """
+    İki tam və ya onluq ədədi toplayır.
+
+    Arqumentlər:
+        a: Birinci ədəd (int və ya float).
+        b: İkinci ədəd (int və ya float), susmaya görə 98.
+
+    Xətalar:
+        TypeError: Əgər a və ya b int/float deyilsə.
+
+    Nəticə:
+        İki ədədin cəmi (int).
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
     return int(a) + int(b)
