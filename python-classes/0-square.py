038bf8e0efa3b4c61bad9ae8919ca02a12e87fe3
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-20 19:00:55 +0400

    Update and rename 1-square.py to 0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Bu modul 'Square' (Kvadrat) sinfini ehtiva edir.
+Bu sinif hələlik boşdur.
+"""
+
+
+class Square:
+    """
+    Kvadratı təmsil edən boş bir sinif.
+    """
+    pass
