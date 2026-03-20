a12a7890926f08a06346a1ce32812d276cec7193
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-20 19:21:18 +0400

    Update and rename 9-rectangle.py to 0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+module with function lookup
+'''
+
+
+def lookup(obj):
+    '''
+    lookup function takes object and returns instance attributes
+    '''
+    return dir(obj)
