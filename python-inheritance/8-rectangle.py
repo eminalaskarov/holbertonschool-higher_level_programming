3a1513915e4aa7b04491c3c3e9e93d370b2d0176
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-20 19:31:31 +0400

    Update 8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-'''
-8-rectangle module with class rectangle
-'''
-
-
+"""
+Bu modul 'Rectangle' (Düzbucaqlı) sinfini ehtiva edir.
+'BaseGeometry' sinfindən miras alır.
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    '''
-    Rectangle class that inherits from BaseGeometry
-    '''
+    """
+    BaseGeometry-dən miras alan Düzbucaqlı sinfi.
+    """
+
     def __init__(self, width, height):
-        self.__width = self.integer_validator('width', width)
-        self.__height = self.integer_validator('height', height)
+        """
+        Yeni bir Rectangle obyekti yaradır.
+
+        Arqumentlər:
+            width (int): Düzbucaqlının eni.
+            height (int): Düzbucaqlının hündürlüyü.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
