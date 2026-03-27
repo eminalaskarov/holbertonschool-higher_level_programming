95d44158305adddfc497fcea522ce3742cba0fba
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-27 21:49:37 +0400

    Update and rename 3-to_json_string.py to 4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+doc for 4-from_json_string module
+'''
+import json
+
+
+def from_json_string(my_str):
+    '''
+    Docstring for from_json_string
+
+    :param my_str: Description
+    '''
+    return json.loads(my_str)
