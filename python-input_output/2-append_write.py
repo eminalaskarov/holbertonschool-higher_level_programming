344cfe9ced4eafc58e050918ae9191577051753d
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-27 21:48:47 +0400

    Update and rename 1-write_file.py to 2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Docstring for python-input_output.2-append_write
+'''
+
+
+def append_write(filename="", text=""):
+    '''
+    Docstring for append_write
+
+    :param filename: Description
+    :param text: Description
+    '''
+    with open(filename, 'a', encoding='UTF-8') as f:
+        f.write(text)
+        return len(text)
