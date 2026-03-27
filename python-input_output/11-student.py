e54c9351616128c02e24c35bb4f639bd3c242803
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-27 22:26:43 +0400

    Update and rename 7-add_item.py to 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Docstring for python-input_output.9-student
+'''
+
+
+class Student:
+    '''
+    Docstring for Student
+    '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            if len(attrs) == 0:
+                return {}
+            return {key: self.__dict__[key] for key in
+                    attrs if key in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = value
