d6beafbdf175b1a227995f05371716152bd9af2b
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-27 21:49:10 +0400

    Update and rename 2-append_write.py to 3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Docstring for python-input_output.3-to_json_string
+'''
+import json
+
+
+def to_json_string(my_obj):
+    '''
+    Docstring for to_json_string
+
+    :param my_obj: Description
+    '''
+    return json.dumps(my_obj)
