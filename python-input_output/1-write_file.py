1128df759a08605befa8c3d842be561211de3a81
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-27 21:48:12 +0400

    Update and rename 0-read_file.py to 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Docstring for python-input_output.1-write_file
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    Docstring for write_file
+
+    :param filename: Description
+    :param text: Description
+    '''
+    with open(filename, 'w', encoding='UTF-8') as f:
+        f.write(text)
+        return len(text)
