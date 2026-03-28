cf4a155e7f06a289e418ab3f5244dcc16b5cfef4
eminalaskarov <aleskerovemin97@gmail.com>
2026-03-28 17:07:33 +0400

    Update and rename task_02_csv.py to task_03_xml.py

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Module for serializing and deserializing a dictionary using XML
+"""
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """
+    Serializes a Python dictionary to XML and saves it to a file
+    """
+    root = ET.Element("data")
+
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding="utf-8", xml_declaration=False)
+
+
+def deserialize_from_xml(filename):
+    """
+    Deserializes XML data from a file and returns a Python dictionary
+    """
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    result = {}
+
+    for child in root:
+        result[child.tag] = child.text
+
+    return result
