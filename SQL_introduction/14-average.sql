ebe152de97cfb7f11d6f6f1aaf9a0908db0ca60c
eminalaskarov <aleskerovemin97@gmail.com>
2026-04-04 14:41:59 +0400

    Update and rename 14-average.sql to 15-groups.sql

--- SQL_introduction/14-average.sql
+++
@@ -1,2 +0,0 @@
--- computes the average score
-SELECT AVG(score) AS average FROM second_table;
---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- counts records grouped by score
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
