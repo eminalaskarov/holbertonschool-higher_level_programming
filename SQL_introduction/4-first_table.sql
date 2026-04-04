6da206aa2e42f44bd5b9ceff73cb246a79735372
eminalaskarov <aleskerovemin97@gmail.com>
2026-04-04 14:29:09 +0400

    Update and rename 4-first_table.sql to 5-full_table.sql

--- SQL_introduction/4-first_table.sql
+++
@@ -1,5 +0,0 @@
--- creates the table first_table if it doesn't exist
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- shows the create statement of first_table
+SHOW CREATE TABLE first_table;
